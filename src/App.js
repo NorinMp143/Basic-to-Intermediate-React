9eda33d781362b3c19e36b047f0a71ba09ba35fc
Madan Lal <madanparmar637@gmail.com>
2021-01-25 23:14:11 +0530

    api + routing done

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import React from "react";
 import { render } from "react-dom";
 import SearchParams from "./SearchParams";
+import { Router, Link } from "@reach/router";
+import Details from "./Details";
 
 const App = () => {
   return (
-    <div>
-      <h1>Adopt Me!</h1>
-      <SearchParams />
-    </div>
+    <React.StrictMode>
+      <div>
+        <header>
+          <Link to="/">Adopt Me!</Link>
+        </header>
+        ;
+        <Router>
+          <SearchParams path="/" />
+          <Details path="/details/:id" />
+        </Router>
+      </div>
+    </React.StrictMode>
   );
 };
 render(<App />, document.getElementById("root"));
