950eaae1cbaf6ba661ba72308dad1170e962e4eb
Madan Lal <madanparmar637@gmail.com>
2021-02-01 01:16:17 +0530

    react testing done by jest

--- src/useDropdown.js
+++ src/useDropdown.js
@@ -7,6 +7,7 @@ const useDropdown = (label, defaultState, options) => {
     <label htmlFor={id}>
       {label}
       <select
+        data-testid={id}
         id={id}
         value={state}
         onChange={(e) => setState(e.target.value)}
